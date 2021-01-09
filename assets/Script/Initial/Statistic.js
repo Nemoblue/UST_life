ab01e0c4aef5b6e56130b8ffec68f86e72570b97
yxiaao <xiayunlei2000@outlook.com>
2021-01-09 21:20:23 +0800

    Add course related file and code

--- assets/Script/Initial/Statistic.js
+++ assets/Script/Initial/Statistic.js
@@ -12,6 +12,8 @@ cc.Class({
     properties: {
         // 选择性别 1->male 0->female
         gender: 0,
+        // gay = "?"
+
 
         // 这些是四种先天属性： 勇气值， 健康值，正义值 ，自信值
         // 随场景保留属性值
@@ -20,6 +22,22 @@ cc.Class({
         health: 0,
         justice: 0,
         confidence: 0,
+
+        // 回合数
+        round: 0,
+        // 上过的课及其成绩，以courseTaken作为obj保存，每门课新增属性，增加分数（课程id：letter)
+        courseTaken: {
+            default: null
+        },
+        // 专业: 新入学的是 'fresh', 日后根据专业变更
+        major: "fresh",
+        // doubleMajor: "null",
+        // 绩点
+        cga: 0,
+        // 总学分
+        credit: 0
+        
+        
     },
 
     // 获取属性 0:bravery 1:health 2:justice 3:confidence 9:gender
@@ -32,6 +50,27 @@ cc.Class({
             case 9: return this.gender; break;
             default: return 0;
         }
+    },
+
+    //获取当前回合数
+    getround(){
+        return this.round;
+    },
+    // 获取绩点
+    getCGA() {
+        return this.cga;
+    },
+    // 获取已经上的课和绩点
+    getCourseTaken() {
+        return this.courseTaken;
+    },
+    // 获取专业
+    getMajor() {
+        return this.major;
+    },
+
+    getCredit(){
+        return this.credit;
     },
 
     // LIFE-CYCLE CALLBACKS:
@@ -68,6 +107,22 @@ cc.Class({
 
     setConfidence(num) {
         this.confidence += num;
+    },
+
+    //集成方法，根据属性名增减数值
+    setPropertiesByName(name, num) {
+
+    },
+
+    //集成方法，根据属性id增减数值
+    setPropertiesById(id, num){
+        switch(id) {
+            case 0: this.bravery += num; break;
+            case 1: this.health += num; break;
+            case 2: this.justice += num; break;
+            case 3: this.confidence += num; break;
+            default: 0;
+        }
     },
 
     // 将属性转换成后天属性
